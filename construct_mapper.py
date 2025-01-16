bb40f0c8a66fa22bb6a56a5e0648109b55a07756
Megan Fairchild <109485286+megankfairchild@users.noreply.github.com>
2025-01-16 08:46:10 -0600

    Update construct_mapper.py

--- construct_mapper.py
+++ construct_mapper.py
@@ -57,47 +57,9 @@ def construct_mapper_graph(data_array, num_intervals, overlap_frac, output_file)
         # Visualize it
         mapper.visualize(graph, path_html="output_men.html", title="output_men")
 
-        """
-        # Configure the Mapper with necessary parameters:
-        graph = mapper.fit_transform(reduced_data, 
-                                     cover_params={'num_intervals': num_intervals, 'overlap_frac': overlap_frac}, 
-                                     clustering_method=DBSCAN(eps=0.5))
-        
-        #Step 3: Visualize the Mapper graph
-        plt.figure(figsize=(10, 10))  # Set the figure size for the plot.
-        nx.draw_networkx(graph, node_size=30, with_labels=False)  # Draw the Mapper graph.
-        plt.savefig(output_file)  # Save the graph to the specified output file.
-        print(f"Mapper graph saved as {output_file}")  # Inform the user about the saved file.
-        """
     except Exception as e:
         print(f"An error occurred: {e}")  # Print any error that occurs.
         raise  # Raise the exception to indicate an error.
-"""
-
-# Import the class
-import kmapper as km
-
-# Some sample data
-from sklearn import datasets
-data, labels = datasets.make_circles(n_samples=5000, noise=0.03, factor=0.3)
-
-# Initialize
-mapper = km.KeplerMapper(verbose=1)
-
-# Fit to and transform the data
-projected_data = mapper.fit_transform(data, projection=[0,1]) # X-Y axis
-
-# Create a cover with 10 elements
-cover = km.Cover(n_cubes=10)
-
-# Create dictionary called 'graph' with nodes, edges and meta-information
-graph = mapper.map(projected_data, data, cover=cover)
-
-# Visualize it
-mapper.visualize(graph, path_html="make_circles_keplermapper_output.html", title="make_circles(n_samples=5000, noise=0.03, factor=0.3)")
-
-
-"""
 
 
 
