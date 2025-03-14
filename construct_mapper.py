75396d3b616c561c7fbfb8c7d8cb6fe4b2c022d0
Megan Fairchild <109485286+megankfairchild@users.noreply.github.com>
2025-03-14 12:30:55 -0500

    Update construct_mapper.py

--- construct_mapper.py
+++ construct_mapper.py
@@ -137,32 +137,6 @@ def construct_mapper_graph_2D(data_array, num_intervals, overlap_frac):
     except Exception as e:
         print(f"An error occurred: {e}")  # Print any error that occurs.
         raise  # Raise the exception to indicate an error.        
-"""
-
-# Import the class
-import kmapper as km
-
-# Some sample data
-from sklearn import datasets
-data, labels = datasets.make_circles(n_samples=5000, noise=0.03, factor=0.3)
-
-# Initialize
-mapper = km.KeplerMapper(verbose=1)
-
-# Fit to and transform the data
-projected_data = mapper.fit_transform(data, projection=[0,1]) # X-Y axis
-
-# Create a cover with 10 elements
-cover = km.Cover(n_cubes=10)
-
-# Create dictionary called 'graph' with nodes, edges and meta-information
-graph = mapper.map(projected_data, data, cover=cover)
-
-# Visualize it
-mapper.visualize(graph, path_html="make_circles_keplermapper_output.html", title="make_circles(n_samples=5000, noise=0.03, factor=0.3)")
-
-
-"""
 
 
 
